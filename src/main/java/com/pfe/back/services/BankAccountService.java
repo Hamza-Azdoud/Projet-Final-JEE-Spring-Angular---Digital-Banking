e2413f42f92202971feefa9425ee52c95fa4a03e
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-06-05 15:02:25 +0100

    la création des compte

--- src/main/java/com/pfe/back/services/BankAccountService.java
+++ src/main/java/com/pfe/back/services/BankAccountService.java
@@ -1,13 +1,17 @@
 package com.pfe.back.services;
 
 import com.pfe.back.entities.BankAccount;
+import com.pfe.back.entities.CurrentAccount;
 import com.pfe.back.entities.Customer;
+import com.pfe.back.entities.SavingAccount;
+import com.pfe.back.exceptions.CustomerNotFoundException;
 
 import java.util.List;
 
 public interface BankAccountService {
     Customer saveCustomer(Customer customer);
-    BankAccount saveBankAccount(double initialBalance, String type, Long customerId);
+    CurrentAccount saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
+    SavingAccount saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
     List<Customer> listCustomer();
     BankAccount getBankAccount(String accountId);
     void debit(String accountId, double amount, String discription);
--- src/main/java/com/pfe/back/services/BankAccountServiceImpl.java
+++ src/main/java/com/pfe/back/services/BankAccountServiceImpl.java
@@ -1,15 +1,21 @@
 package com.pfe.back.services;
 
 import com.pfe.back.entities.BankAccount;
+import com.pfe.back.entities.CurrentAccount;
 import com.pfe.back.entities.Customer;
+import com.pfe.back.entities.SavingAccount;
+import com.pfe.back.exceptions.CustomerNotFoundException;
 import com.pfe.back.repository.AccountOperationRepository;
+import com.pfe.back.repository.BankAccountRepository;
 import com.pfe.back.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 @Service
@@ -18,6 +24,7 @@ import java.util.logging.Logger;
 public class BankAccountServiceImpl implements BankAccountService{
 
     private CustomerRepository customerRepository;
+    private BankAccountRepository bankAccountRepository;
     private BankAccountService bankAccountService;
     private AccountOperationRepository accountOperationRepository;
     Logger log = (Logger) LoggerFactory.getLogger(this.getClass().getName());
@@ -25,12 +32,43 @@ public class BankAccountServiceImpl implements BankAccountService{
     @Override
     public Customer saveCustomer(Customer customer) {
         log.info("Saveng new Custmer");
-        return null;
+        Customer saveCustomer = customerRepository.save(customer);
+        return saveCustomer;
     }
 
     @Override
-    public BankAccount saveBankAccount(double initialBalance, String type, Long customerId) {
-        return null;
+    public CurrentAccount saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException {
+
+        Customer customer= customerRepository.findById(customerId).orElse(null);
+        if (customer == null){
+            throw  new CustomerNotFoundException("Customer not found");
+        }
+        CurrentAccount currentAccount = new CurrentAccount();
+        currentAccount.setId(UUID.randomUUID().toString());
+        currentAccount.setCreatedAt(new Date());
+        currentAccount.setBalance(initialBalance);
+        currentAccount.setCustomer(customer);
+        currentAccount.setOverDraft(overDraft);
+
+        CurrentAccount saveBankAccount = bankAccountRepository.save(currentAccount);
+        return saveBankAccount;
+    }
+
+    @Override
+    public SavingAccount saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException {
+        Customer customer= customerRepository.findById(customerId).orElse(null);
+        if (customer == null){
+            throw  new CustomerNotFoundException("Customer not found");
+        }
+        SavingAccount savingAccount = new SavingAccount();
+        savingAccount.setId(UUID.randomUUID().toString());
+        savingAccount.setCreatedAt(new Date());
+        savingAccount.setBalance(initialBalance);
+        savingAccount.setCustomer(customer);
+        savingAccount.setInterestRate(interestRate);
+
+        SavingAccount saveBankAccount = bankAccountRepository.save(savingAccount);
+        return saveBankAccount;
     }
 
     @Override
